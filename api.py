77733ad6e3e612273f76609b3e28cb34836522c9
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-07-15 13:55:44 +0530

    Update api.py

--- api.py
+++ api.py
@@ -38,6 +38,10 @@ def classify():
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
     clf.score(X_test, y_test)
+     # Alternative Usage of Saved Model
+    # joblib.dump(clf, 'NB_spam_model.pkl')
+    # NB_spam_model = open('NB_spam_model.pkl','rb')
+    # clf = joblib.load(NB_spam_model)
    
 
     if request.method == 'GET':
