4d28c1f2d02370877d89a18e4eb2c5b1995b654c
Raj Kishore Sahni <63728002+raj9650sahni@users.noreply.github.com>
2021-07-15 13:05:36 +0530

    Update api.py

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for spam classification.</p>"
 
 
 @app.route('/classify', methods=['GET'])
